d5ae67165e7d1e91b103a02f710bc6c09b899aae
LaurentCOLL1 <128934707+LaurentCOLL1@users.noreply.github.com>
2026-08-21 19:55:59 +0200

    r4.2: export parser services

--- src/kodepoia/kodecode/__init__.py
+++ src/kodepoia/kodecode/__init__.py
@@ -3,18 +3,44 @@
 from kodepoia.kodecode.api import KodeCodeToolAPI
 from kodepoia.kodecode.files import FileTool
 from kodepoia.kodecode.git_worktree import GitWorktreeTool, WorktreeInfo
+from kodepoia.kodecode.parser_tool import ParserTool
+from kodepoia.kodecode.parsing import (
+    ChangedRangeSnapshot,
+    IncrementalParseResult,
+    IncrementalParseSession,
+    LanguageCapability,
+    LanguageProviderSpec,
+    ParseDocument,
+    ParseSummary,
+    SyntaxNodeSnapshot,
+    TreeSitterLanguageRegistry,
+    TreeSitterParserService,
+    TreeSitterUnavailable,
+)
 from kodepoia.kodecode.patch import PatchResult, PatchTool
 from kodepoia.kodecode.search import SearchMatch, SearchTool
 from kodepoia.kodecode.workspace import WorkspaceBoundary, WorkspaceViolation
 
 __all__ = [
+    "ChangedRangeSnapshot",
     "FileTool",
     "GitWorktreeTool",
+    "IncrementalParseResult",
+    "IncrementalParseSession",
     "KodeCodeToolAPI",
+    "LanguageCapability",
+    "LanguageProviderSpec",
+    "ParseDocument",
+    "ParseSummary",
+    "ParserTool",
     "PatchResult",
     "PatchTool",
     "SearchMatch",
     "SearchTool",
+    "SyntaxNodeSnapshot",
+    "TreeSitterLanguageRegistry",
+    "TreeSitterParserService",
+    "TreeSitterUnavailable",
     "WorkspaceBoundary",
     "WorkspaceViolation",
     "WorktreeInfo",
